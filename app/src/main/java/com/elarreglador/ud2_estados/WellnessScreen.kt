6197c2d489c9468749d39a6addd19b881dbf35ab
elarreglador <elarreglador@protonmail.com>
2024-10-21 20:33:42 +0200

    Tenemos un listado de tareas, al salir de pantalla se reinicia el valor con remember pero no con rememberSaveable en WellnessTaskItem

--- app/src/main/java/com/elarreglador/ud2_estados/WellnessScreen.kt
+++ app/src/main/java/com/elarreglador/ud2_estados/WellnessScreen.kt
@@ -1,8 +1,13 @@
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.elarreglador.ud2_estados.StatefulCounter
+import com.elarreglador.ud2_estados.WellnessTasksList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    StatefulCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList()
+    }
 }
---
+++ app/src/main/java/com/elarreglador/ud2_estados/WellnessTask.kt
@@ -0,0 +1,3 @@
+package com.elarreglador.ud2_estados
+
+data class WellnessTask(val id: Int, val label: String)
