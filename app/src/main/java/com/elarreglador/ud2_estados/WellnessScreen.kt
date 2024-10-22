b25ba408f16525200845b356085957c8a7e6830c
elarreglador <elarreglador@protonmail.com>
2024-10-22 07:22:57 +0200

    Funcionalidad de cierre de tareas y centrado del boton

--- app/src/main/java/com/elarreglador/ud2_estados/WellnessScreen.kt
+++ app/src/main/java/com/elarreglador/ud2_estados/WellnessScreen.kt
@@ -1,13 +1,26 @@
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.elarreglador.ud2_estados.StatefulCounter
+import com.elarreglador.ud2_estados.WellnessTask
 import com.elarreglador.ud2_estados.WellnessTasksList
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
+    Column(modifier = modifier,
+        Arrangement.Top,
+        Alignment.CenterHorizontally) {
         StatefulCounter()
-        WellnessTasksList()
+
+        val list = remember { getWellnessTasks().toMutableStateList() }
+        WellnessTasksList(list = list, onCloseTask = { task -> list.remove(task) })
     }
+}
+
+private fun getWellnessTasks() = List(30) {
+    i -> WellnessTask(i, "Task # $i")
 }
